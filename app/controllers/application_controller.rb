86d1e671f2b037f65f6d54651ea60855556a26e2
LIFE-enjoy <hndke10ve@icloud.com>
2021-03-23 11:51:44 +0900

    Basic認証の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+  private
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'kenjihonda' && password == 'hndke9ca'
+    end
+  end
 end
