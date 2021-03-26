12a54bc5f8aa1f0f31a63e98f82edb9da63824f1
LIFE-enjoy <hndke10ve@icloud.com>
2021-03-26 19:21:22 +0900

    新規登録での名前入力のバリデーションを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ApplicationRecord
 
  with_options presence: true do
     validates :nickname
-    validates :last_name, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角かな/カナ/漢字で入力してください",allow_blank: true}
-    validates :first_name, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角かな/カナ/漢字で入力してください",allow_blank: true}
+    validates :last_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "は全角かな/カナ/漢字で入力してください",allow_blank: true}
+    validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "は全角かな/カナ/漢字で入力してください",allow_blank: true}
     validates :last_name_kana, format: {with: /\A[ァ-ヶー－]+\z/, message: "は全角カナで入力してください",allow_blank: true}
     validates :first_name_kana, format: {with: /\A[ァ-ヶー－]+\z/, message: "は全角カナで入力してください",allow_blank: true}
     validates :birth_date
