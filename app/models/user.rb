2eea250eb38f4f15a877c08185a80659cda15e64
LIFE-enjoy <hndke10ve@icloud.com>
2021-03-26 13:17:58 +0900

    エラーメッセージの重複防止の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,12 @@ class User < ApplicationRecord
 
  with_options presence: true do
     validates :nickname
-    validates :last_name, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角かな/カナ/漢字で入力してください"}
-    validates :first_name, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角かな/カナ/漢字で入力してください"}
-    validates :last_name_kana, format: {with: /\A[ァ-ヶー－]+\z/, message: "は全角カナ入力してください"}
-    validates :first_name_kana, format: {with: /\A[ァ-ヶー－]+\z/, message: "は全角カナ入力してください"}
+    validates :last_name, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角かな/カナ/漢字で入力してください",allow_blank: true}
+    validates :first_name, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角かな/カナ/漢字で入力してください",allow_blank: true}
+    validates :last_name_kana, format: {with: /\A[ァ-ヶー－]+\z/, message: "は全角カナ入力してください",allow_blank: true}
+    validates :first_name_kana, format: {with: /\A[ァ-ヶー－]+\z/, message: "は全角カナ入力してください",allow_blank: true}
     validates :birth_date
     validates :password_confirmation
  end
-    validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "は半角英数字で入力してください"}, confirmation: true
+    validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "は半角英数字を含めて入力してください",allow_blank: true}
 end
