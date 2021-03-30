a4de0ece58b0a1991de368226b2038ff8121a1cb
LIFE-enjoy <hndke10ve@icloud.com>
2021-03-30 11:47:54 +0900

    JavaScriptによる金額の計算結果表示の実装

---
+++ app/javascript/item_price.js
@@ -0,0 +1,13 @@
+function itemPrice() {
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener('input', () => {
+    const inputValue = priceInput.value;
+    const addTaxDom = document.getElementById("add-tax-price");
+    const salesProfit = document.getElementById("profit")
+    addTaxDom.innerHTML = (Math.floor(inputValue * 0.1));
+    salesProfit.innerHTML = (Math.floor(inputValue * 0.9));
+  })
+}
+
+
+window.addEventListener('load',itemPrice)
