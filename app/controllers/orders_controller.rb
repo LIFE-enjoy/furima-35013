d26d3e9e136e25e5e1bfd864fbe3f1a859a9bfc3
LIFE-enjoy <hndke10ve@icloud.com>
2021-04-01 21:23:53 +0900

    formオブジェクトの実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,35 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item, only: [:index, :create]
+  before_action :redirect_root
+
   def index
+    @order_record_order = OrderRecordOrder.new
+  end
+
+  def create
+    @order_record_order = OrderRecordOrder.new(order_params)
+    if @order_record_order.valid?
+      @order_record_order.save
+      redirect_to root_path
+    else
+      render action: :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_record_order).permit(
+      :postal_code, :prefecture_id, :city, :address, :building, :phone_number
+    ).merge(user_id: current_user.id, item_id: @item.id)
+  end
+
+  def set_item
     @item = Item.find(params[:item_id])
   end
 
-  
+  def redirect_root
+    redirect_to root_path if current_user.id == @item.user_id
+  end
 end
--- app/models/order_record_order.rb
+++ app/models/order_record_order.rb
@@ -1,22 +1,22 @@
 class OrderRecordOrder
 
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :user_id, :item_id
+  attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :order_record_id, :user_id, :item_id
 
   with_options presence: true do
-    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ ,message: 'に-(ハイフン)をつけてください',allow_blank: true}
     validates :city
     validates :address
     validates :phone_number
     validates :user_id
     validates :item_id
+  
+    validates :prefecture_id, numericality: { other_than: 0, message: 'を選択してください' }
   end
-  validates :prefecture_id, numericality: { other_than: 0, message: 'を選択してください' }
-
   def save
-    order = Order.create(
-      postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building: building, phone_number: phone_number
-    )
     order_record = OrderRecord.create(user_id: user_id, item_id: item_id)
+    Order.create(
+      postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building: building, phone_number: phone_number, order_record_id: order_record.id
+    )
   end
 end
