f5e8426e1f62521222a1961c9aede51cf7273003
LIFE-enjoy <hndke10ve@icloud.com>
2021-04-01 12:41:12 +0900

    アソシエーションの設定

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,6 @@
 class Order < ApplicationRecord
+  belongs_to :order_record
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
 end
--- app/models/order_record.rb
+++ app/models/order_record.rb
@@ -1,2 +1,5 @@
 class OrderRecord < ApplicationRecord
+  has_one :order
+  belongs_to :user
+  belongs_to :item
 end
