70d7334ffe688d6accd464d937fc806d917a4318
LIFE-enjoy <hndke10ve@icloud.com>
2021-04-02 19:24:53 +0900

    商品詳細ページにて売却済みによる条件分岐の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,10 +2,10 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :redirect_root, only: [:edit, :update, :destroy]
+  before_action :set_order_record, only: [:index, :show]
 
   def index
     @items = Item.all.order('created_at DESC')
-    @order_record = OrderRecord.includes(:item)
   end
 
   def new
@@ -54,5 +54,9 @@ class ItemsController < ApplicationController
 
   def redirect_root
     redirect_to root_path unless current_user.id == @item.user_id
+  end
+
+  def set_order_record
+    @order_record = OrderRecord.all
   end
 end
