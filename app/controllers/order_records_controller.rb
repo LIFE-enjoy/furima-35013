757537a33f86c7fcdf4cdccae3f5f8e913d6a98c
LIFE-enjoy <hndke10ve@icloud.com>
2021-05-03 10:25:02 +0900

    注文情報一覧ページにauthenticate_userメソッドを追加

--- app/controllers/order_records_controller.rb
+++ app/controllers/order_records_controller.rb
@@ -1,4 +1,6 @@
 class OrderRecordsController < ApplicationController
+  before_action :authenticate_user!
+  
   def index
     @order_record = OrderRecord.where(user_id: current_user.id).order('created_at DESC')
   end
