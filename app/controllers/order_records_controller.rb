f0d8cf77c5a274bd0295a4b413120d00bf3f4bf4
LIFE-enjoy <hndke10ve@icloud.com>
2021-05-03 12:19:35 +0900

    注文履歴ページへのURL直打ち防止の措置の記述

--- app/controllers/order_records_controller.rb
+++ app/controllers/order_records_controller.rb
@@ -1,7 +1,14 @@
 class OrderRecordsController < ApplicationController
   before_action :authenticate_user!
+  before_action :redirect_root
   
   def index
     @order_record = OrderRecord.where(user_id: current_user.id).order('created_at DESC')
+  end
+
+  private
+  def redirect_root
+    @user = User.find(params[:user_id])
+    redirect_to root_path if current_user.id != @user.id
   end
 end
